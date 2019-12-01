afb13d77710722a73d7d8a6e5e3715cb98680d8d
David Hodgson <dahodgson@gmail.com>
2019-12-01 09:10:40 +0000

    Added File Parser

---
+++ fileparser.go
@@ -0,0 +1,37 @@
+package adventofcode2019
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+func ReadFile(filename string) string {
+
+	file, err := os.Open(filename) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var contents string
+
+	if file != nil {
+		buffer := make([]byte, 1024)
+		for {
+
+			count, err := file.Read(buffer)
+			if err != nil {
+				break
+			} else {
+				contents += string(buffer[0:count])
+			}
+
+		}
+	}
+
+	if strings.HasSuffix(contents, "\n") {
+		contents = contents[0 : len(contents)-1]
+	}
+
+	return contents
+}
