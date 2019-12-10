df65684c54288dcc9df6ca2219fb857a0d30f954
dahodgson <dahodgson@qinetiq.com>
2019-12-10 16:11:53 +0000

    Day 6 - Part Two - Complete

--- DaySix.go
+++ DaySix.go
@@ -14,6 +14,89 @@ func DaySixPartOne() {
 	fmt.Println("total: ", total)
 }
 
+func DaySixPartTwo() {
+	fmt.Println("2019 - Day Six - Part Two")
+
+	input := strings.Split(ReadFile("day6-2019-input.txt"), "\n")
+	tree := buildTree(input)
+
+	depthMap := buildDepthMap(tree, "COM", 0)
+	youDepth := depthMap["YOU"]
+	sanDepth := depthMap["SAN"]
+	pathCount := generatePathCount(tree,depthMap,"YOU","SAN")
+	fmt.Println("Path Count: ", pathCount)
+}
+
+func generatePathCount(tree map[string][]string,depthMap map[string]int, node1 string, node2 string) int {
+
+	node1Depth := depthMap[node1]
+	node2Depth := depthMap[node2]
+
+	if node1Depth == node2Depth {
+
+		//nodes are at the same depth
+		n1Parent := getNodeParent(node1,tree)
+		n2Parent := getNodeParent(node2,tree)
+
+		if n1Parent == n2Parent {
+			//Same Parent
+			return 0
+		} else {
+			return 2 + generatePathCount(tree,depthMap, n1Parent,n2Parent)
+		}
+
+	} else {
+
+		//Mismatched nodes move the lowest one up
+		if (node1Depth < node2Depth) {
+			//move node 2
+			n2Parent := getNodeParent(node2,tree)
+			return 1 + generatePathCount(tree,depthMap, node1,n2Parent)
+		} else {
+			//move node 1
+			n1Parent := getNodeParent(node1,tree)
+			return 1 + generatePathCount(tree,depthMap, n1Parent,node2)
+		}
+	}
+
+	return -1
+
+}
+
+func buildDepthMap(tree map[string][]string, root string, currentDepth int) map[string]int {
+
+	depthMap := make(map[string]int)
+
+	depthMap[root] = currentDepth
+
+	children := tree[root]
+
+	for i := 0; i < len(children); i++ {
+	
+		childMap := buildDepthMap(tree, children[i], currentDepth+1)
+
+		for k,v := range childMap {
+			depthMap[k] = v
+		}
+	}
+
+	return depthMap
+}
+
+func getNodeParent(node string, tree map[string][]string) string {
+	for k,v := range tree {
+
+		for i := 0; i <len (v); i++ {
+			if v[i] == node {
+				return k
+			}
+		}
+
+	}
+
+	return "nil"
+}
+
 func calculateOrbits(tree map[string][]string, start string, depth int) int {
 
 	total := 0
