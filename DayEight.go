4ed2868e15142526a4775980a72cebd9de70c93e
David Hodgson <dahodgson@gmail.com>
2019-12-11 19:11:59 +0000

    Day Eight - Part Two - Complete

--- DayEight.go
+++ DayEight.go
@@ -8,7 +8,6 @@ import (
 func DayEightPartOne() {
 
 	fmt.Println("2019 - Day Eight - Part One")
-
 	input := ReadFile("day8-2019-input.txt")
 
 	width := 25
@@ -28,10 +27,10 @@ func DayEightPartOne() {
 
 		layerStart := layer * width * height
 		layerEnd := layerStart + (width * height)
-
 		layerValues := input[layerStart:layerEnd]
 
 		for i := 0; i < len(layerValues); i++ {
+
 			colour, _ := strconv.Atoi(string(layerValues[i]))
 
 			switch colour {
@@ -49,6 +48,63 @@ func DayEightPartOne() {
 			total = oneCount * twoCount
 		}
 	}
-
 	fmt.Println("Total: ", total)
+}
+
+func DayEightPartTwo() {
+
+	fmt.Println("2019 - Day Eight - Part Two")
+
+	input := ReadFile("day8-2019-input.txt")
+
+	width := 25
+	height := 6
+
+	layerCount := len(input) / (width * height)
+
+	fmt.Println("Layer Count:", layerCount)
+
+	finalLayer := make([]int, height*width)
+
+	for i := 0; i < len(finalLayer); i++ {
+		finalLayer[i] = -1
+	}
+
+	printLayer(finalLayer, height, width)
+
+	for layer := 0; layer < layerCount; layer++ {
+
+		layerStart := layer * width * height
+		layerEnd := layerStart + (width * height)
+		layerValues := input[layerStart:layerEnd]
+
+		for i := 0; i < len(layerValues); i++ {
+
+			value, _ := strconv.Atoi(string(layerValues[i]))
+
+			if finalLayer[i] == -1 && value != 2 {
+				finalLayer[i] = value
+			}
+		}
+
+		printLayer(finalLayer, height, width)
+		fmt.Println("")
+	}
+}
+
+func printLayer(layer []int, height int, width int) {
+
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+
+			cell := layer[(row*width)+col]
+
+			if cell != -1 && cell != 0 {
+				fmt.Print(cell)
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 }
